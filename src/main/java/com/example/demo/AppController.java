4fca772f5fd5c3b732e1125142d56789eb3f3ab2
jannatunnayeem <nayeemmdjannatun@gmail.com>
2023-09-26 11:23:38 +0600

    Locally done with postman CRUD

--- src/main/java/com/example/demo/AppController.java
+++ src/main/java/com/example/demo/AppController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,45 +20,55 @@ public class AppController {
   @Autowired
   private CustomerRepository customerRepository;
 
-//  @PostMapping(path = "/customer" )
   @PostMapping(path = "/postCustomer")
-  public ResponseEntity<String> addCustomer(@RequestBody Customer customer) throws Exception {
+  public ResponseEntity<String> postCustomer(@RequestBody Customer customer) throws Exception {
     customerRepository.save(customer);
+
     System.out.println("----===-+++++ customer : "+customer);
+
     return new ResponseEntity<>("OK", HttpStatus.OK);
   }
 
-  @GetMapping("/getCustomer")
-  public ResponseEntity<Customer> getCustomer(@RequestParam("id") String id) {
+  @GetMapping("/getCustomerById")
+  public ResponseEntity<Customer> getCustomerById(@RequestParam("id") String id) {
     Optional<Customer> customer = customerRepository.findById(Long.parseLong(id));
     return new ResponseEntity<>(customer.isPresent() ? customer.get() : null, HttpStatus.OK);
   }
 
-  @DeleteMapping("/deleteCustomer")
-  public void deleteCustomer(@RequestParam("id") String id) {
-    if(this.customerRepository.findById(Long.parseLong(id))!=null) {
-      customerRepository.deleteById(Long.parseLong(id));
+  @GetMapping("/getCustomer")
+  public Iterable<Customer> getCustomer() {
+    Iterable<Customer> customerList;
+    customerList = customerRepository.findAll();
+    if(customerList!=null) {
+      return  customerList;
     }
     else {
-      System.out.println("Id not found+++++----");
+      return null;
     }
+  }
 
+  @DeleteMapping("/deleteCustomerById/{id}")
+  public String deleteCustomer(@PathVariable Long id) {
+    if(this.customerRepository.findById(id)!=null) {
+      customerRepository.deleteById((id));
+      return "Deteted Successfully";
+    }
+    else {
+      return "Error";
+    }
   }
 
     @PutMapping("/putCustomer")
-    public ResponseEntity<String> putCustomer(@RequestBody Customer customer) throws Exception {
+    public String putCustomer(@RequestBody Customer customer) {
       System.out.println("----===-+++++ customer : "+customer);
-      return new ResponseEntity<>("OK", HttpStatus.OK);
+      Long id = customer.getId();
+      boolean isPresent = customerRepository.existsById(id);
+      if(isPresent==false) {
+        return  "Id not exists";
+      }
+      customerRepository.deleteById(id);
+      customerRepository.save(customer);
+      return "Updated Successfully";
     }
-
-  //    @PutMapping("/{id}")
-//    public Book updateBook(@PathVariable Long id, @RequestBody Book updatedBook) {
-//        return bookService.updateBook(id,updatedBook);
-//    }
-
-//  @GetMapping(path = "/getCustomer")
-//  public ResponseEntity<Customer> (@RequestParam("id") Long id) throws Exception {
-//
-//  }
 
 }
